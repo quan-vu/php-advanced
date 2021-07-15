b3606ee4b236c0638610ede398bb8f2951886d8e
Quan Vu <huyquan.vu.1@gmail.com>
2021-07-15 10:18:46 +0700

    [Interface] implements multiple interfaces

--- interfaces/Interface.php
+++ interfaces/Interface.php
@@ -4,10 +4,19 @@ interface TableInterface {
     public function save(array $data);
 }
 
-class Table implements TableInterface {
+interface LogInterface {
+    public function log($message);
+}
+
+class Table implements TableInterface, LogInterface {
     public function save(array $data) {
         return 'foo';
+    }
+
+    public function log($message) {
+        return $message . "\n";
     }
 }
 
 echo (new Table())->save([]);
+echo (new Table())->log(' interfaces are awesome.');
