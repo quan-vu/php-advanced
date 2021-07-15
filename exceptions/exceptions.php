ba4b5085554be183b5684bbc1669e23a123f5b55
Quan Vu <huyquan.vu.1@gmail.com>
2021-07-15 22:52:13 +0700

    [PHP Exceptions] - Nested exception

--- exceptions/exceptions.php
+++ exceptions/exceptions.php
@@ -1,14 +1,39 @@
 <?php
 
+// Throw an exception
+/*
 try {
     processCreditCard();
 } catch (Exception $e) {
     echo $e->getMessage();
 }
-
 function processCreditCard($numb = null, $zipCode = null) {
     if (is_null($numb)) {
         throw new Exception('No credit card number');
     }
     echo 'proccessed';
+}
+*/
+
+// Nested exception
+try {
+    processCreditCard();
+} catch (Exception $e) {
+    echo $e->getMessage();
+    echo "\n";
+    echo $e->getPrevious()->getMessage();
+    echo "\n";
+}
+function processCreditCard($numb = null, $zipCode = null) {
+    try {
+        validate($numb, $zipCode);
+    } catch (Exception $e) {
+        throw new Exception('Invalid Inputs', null, $e);
+    }
+    echo 'proccessed';
+}
+function validate($numb, $zipCode) {
+    if (is_null($numb)) {
+        throw new Exception('No credit card number');
+    }
 }
